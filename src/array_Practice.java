756b50fced0bfc0828e1df8feacdbc4dc7c9527b
jenniatz <71726141+jenniatz@users.noreply.github.com>
2021-09-21 19:16:21 -0400

    Add files via upload
    demo commit

---
+++ src/array_Practice.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class array_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int [] values = {17, -1 , -1 , -1 , -1 ,-1 , -1 };
+		
+		System.out.println(values);
+		
+		
+			//values [i] = -1;
+			
+			
+			
+			
+			
+			
+			
+			
+				
+			
+			
+		
+				 
+		}	
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	
+
+}
